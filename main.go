78848637756492d8138884c867953808e64828c0
jubeiam <jubeiam@gmail.com>
2023-03-12 20:33:11 +0100

    feat: use nbio websocket package

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"runtime"
 	"sync/atomic"
 	"time"
@@ -49,6 +51,16 @@ func middlewareContext(next http.HandlerFunc) http.HandlerFunc {
 	})
 }
 
+func middlewareLogger(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		log.Println(r.URL.Path, fmt.Sprintf("%d ms", time.Since(start).Milliseconds()))
+	})
+}
+
 func main() {
 	flag.Parse()
 
@@ -56,9 +68,8 @@ func main() {
 
 	// mux := mux.NewRouter()
 	// apiRouter(mux.PathPrefix("/api").Subrouter())
-	// wsRouter(mux.PathPrefix("/ws").Subrouter())
-
-	mux.HandleFunc("/", middlewareContext(http.HandlerFunc(serveHome)))
+	mux.HandleFunc("/", middlewareLogger(http.HandlerFunc(serveHome)))
+	mux.HandleFunc("/ws", middlewareLogger(onWebsocket))
 
 	// Run our server in a goroutine so that it doesn't block.
 	svr := nbhttp.NewServer(nbhttp.Config{
@@ -72,7 +83,6 @@ func main() {
 		fmt.Printf("nbio.Start failed: %v\n", err)
 		return
 	}
-	defer svr.Stop()
 
 	delta := 5
 	ticker := time.NewTicker(time.Second * time.Duration(delta))
@@ -80,6 +90,13 @@ func main() {
 		<-ticker.C
 		n := atomic.SwapUint64(&qps, 0)
 		total += n
-		fmt.Printf("running for %v seconds, NumGoroutine: %v, qps: %v, total: %v\n", i*delta, runtime.NumGoroutine(), n/uint64(delta), total)
+		log.Printf("running for %v seconds, NumGoroutine: %v, qps: %v, total: %v\n", i*delta, runtime.NumGoroutine(), n/uint64(delta), total)
 	}
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	svr.Shutdown(ctx)
 }
