7b0b3b46019f4a9610ef028317aeb9cc552fee02
jubeiam <jubeiam@gmail.com>
2023-03-12 12:13:12 +0100

    feat: gorilla -> nbio

--- go.mod
+++ go.mod
@@ -8,4 +8,10 @@ require github.com/gorilla/websocket v1.5.0
 
 require gopkg.in/sorcix/irc.v2 v2.0.0-20200812151606-3f15758ea8c7
 
-require github.com/thoas/go-funk v0.9.2
+require github.com/lesismal/nbio v1.3.10
+
+require (
+	github.com/lesismal/llib v1.1.10 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
+	"runtime"
+	"sync/atomic"
 	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/lesismal/nbio/nbhttp"
 )
 
 func getPort() string {
@@ -21,6 +21,11 @@ func getPort() string {
 
 	return "8080"
 }
+
+var (
+	qps   uint64 = 0
+	total uint64 = 0
+)
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
@@ -35,55 +40,46 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
+func middlewareContext(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("middlewareContext")
+
+		atomic.AddUint64(&qps, 1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
 	flag.Parse()
 
-	mux := mux.NewRouter()
-	apiRouter(mux.PathPrefix("/api").Subrouter())
-	wsRouter(mux.PathPrefix("/ws").Subrouter())
+	mux := &http.ServeMux{}
 
-	mux.HandleFunc("/", serveHome)
+	// mux := mux.NewRouter()
+	// apiRouter(mux.PathPrefix("/api").Subrouter())
+	// wsRouter(mux.PathPrefix("/ws").Subrouter())
 
-	port := getPort()
-
-	addr := fmt.Sprintf("0.0.0.0:%s", port)
-
-	srv := &http.Server{
-		Addr: addr,
-		// Good practice to set timeouts to avoid Slowloris attacks.
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      mux, // Pass our instance of gorilla/mux in.
-	}
+	mux.HandleFunc("/", middlewareContext(http.HandlerFunc(serveHome)))
 
 	// Run our server in a goroutine so that it doesn't block.
-	go func() {
-		log.Printf("Start server: %s", addr)
-		if err := srv.ListenAndServe(); err != nil {
-			log.Println(err)
-		}
-	}()
+	svr := nbhttp.NewServer(nbhttp.Config{
+		Network: "tcp",
+		Addrs:   []string{"localhost:" + getPort()},
+		Handler: mux,
+	}) // pool.Go)
 
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
+	err := svr.Start()
+	if err != nil {
+		fmt.Printf("nbio.Start failed: %v\n", err)
+		return
+	}
+	defer svr.Stop()
 
-	// Block until we receive our signal.
-	<-c
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
-	srv.Shutdown(ctx)
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
-	log.Println("shutting down")
-	os.Exit(0)
+	delta := 5
+	ticker := time.NewTicker(time.Second * time.Duration(delta))
+	for i := 1; true; i++ {
+		<-ticker.C
+		n := atomic.SwapUint64(&qps, 0)
+		total += n
+		fmt.Printf("running for %v seconds, NumGoroutine: %v, qps: %v, total: %v\n", i*delta, runtime.NumGoroutine(), n/uint64(delta), total)
+	}
 }
